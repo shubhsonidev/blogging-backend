737226afd4173ebaae71ac2041f4b064874f47e9
Shubh soni <shubhsoni.dev@gmail.com>
2024-07-14 22:47:02 +0530

    fix

--- routes/blog.js
+++ routes/blog.js
@@ -9,7 +9,7 @@ const router = express.Router();
 // Creating storage using multer
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.resolve("/tmp/"));
+    cb(null, os.tmpdir());
   },
   filename: function (req, file, cb) {
     const fileName = `${Date.now()}-${file.originalname}`;
